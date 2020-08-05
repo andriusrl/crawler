ca4b25a0ac7ea3eee00723bdbb05089c4117ebc1
Andrius <andrius.rochalazarino@gmail.com>
2020-08-05 08:36:56 -0400

    Finalizado

--- src/business/ProductBusiness.ts
+++ src/business/ProductBusiness.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export class ProductBusiness {
-    public async getProduct(urlPage: string): Promise<string> {
+    public async getProduct(urlPage: string): Promise<Product> {
         const position1: number | undefined = urlPage?.indexOf(".br/", 1) + 3
         const position2: number | undefined = urlPage?.indexOf(".html", 1) + 5
         const requestUrl: string = process.env.URL_API + urlPage?.slice(position1, position2)
@@ -15,13 +15,12 @@ export class ProductBusiness {
                 return res.json()
             })
 
-        return JSON.stringify((
-            new Product(
-                response.disponibilidade.nome,
-                parseFloat(response.precos[0].valor.replace(',', '.')),
-                response.imagens[0].urls.filter((image: { resolucao: string; }) => { return image.resolucao === "500x500" })[0].url,
-                response.disponibilidade.cores.map((cor: { nomeCor: string; }) => { return cor.nomeCor })
-            )
-        ))
+        return new Product(
+            response.disponibilidade.nome,
+            parseFloat(response.precos[0].valor.replace(',', '.')),
+            response.imagens[0].urls.filter((image: { resolucao: string; }) => { return image.resolucao === "500x500" })[0].url,
+            response.disponibilidade.cores.map((cor: { nomeCor: string; }) => { return cor.nomeCor })
+        )
+
     }
 }
--- src/controller/ProductController.ts
+++ src/controller/ProductController.ts
@@ -3,8 +3,13 @@ import { ProductBusiness } from "../business/ProductBusiness";
 export class ProductController {
     async getProduct(urlPage: string){
         try {
-            const result = new ProductBusiness().getProduct(urlPage)
-            console.log(await result)
+            const result = await (new ProductBusiness().getProduct(urlPage))
+            console.log(JSON.stringify({
+                "titulo": result.getTitle(),
+                "preco": result.getPrice(),
+                "imagem": result.getImage(),
+                "coresDisponiveis": result.getAvaibleColors()
+            }))
         } catch (err){
             console.log(err)
         }
